c6257eb31fd468313a74adcad4e9bb45d0027548
Astro13245 <121739164+Astro13245@users.noreply.github.com>
2023-04-20 19:36:11 -0400

    Added prompts to the create function

--- test.js
+++ test.js
@@ -1,13 +1,37 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
-let unicellularOrganisms = {}
+let unicellularOrganisms = {};
 
-let multicellularOrganisms = {}
+let multicellularOrganisms = {};
 
 function create(){
+    console.log("You can only create unicellular organisms(sorry)")
+    let name = prompt("What's the name of the organism");
+    let type = prompt("What's the type(Prokaryotes/Eukaryotes): ");
+    unicellularOrganisms[name] = {
+        type: type,
+
+    };
+
+    let startPlay = prompt("Do you want to start")
+
+    if (startPlay.toLowerCase() === "yes") {
+        play();
+    }
+};
+
+function eat(eatingOrganism, beingEatenOrganism) {
 
 }
 
-function reproduce(){
+function reproduce(organism){
 
-}
+};
+
+function play(){
+    for (let i = 0; i < unicellularOrganisms.length; i++){
+        unicellularOrganisms[i].numberOfOrganisms++;
+    };
+
+    
+};
